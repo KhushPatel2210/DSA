3b490cb69a348eed4a08cb5e4fa3e744da6e39ab
khush patel <khushp2210@gmail.com>
2025-11-25 16:13:47 +0530

    basic math

--- Basic Math/ReverseNumber.js
+++ Basic Math/ReverseNumber.js
@@ -1,14 +1,20 @@
 var reverse = function (x) {
   let reverseno = 0;
 
+  // sign used to store whether the number is negative or positive
   let sign = x < 0 ? -1 : 1;
+
+  //math.abs remove the negative sign
   x = Math.abs(x);
 
   while (x > 0) {
+    // getting last digit
     let last = x % 10;
+    // removing last digit from x
     x = Math.floor(x / 10); // FIXED
+    // appending last digit to reverseno
     reverseno = reverseno * 10 + last;
   }
-
+  // returning the reversed number with its sign
   return sign * reverseno;
 };
