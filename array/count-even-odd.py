6dc83ee15f35c44ef2ead7e6010a4e04f332d50f
khush patel <khushp2210@gmail.com>
2026-04-17 15:39:27 +0530

    comments

--- array/count-even-odd.py
+++ array/count-even-odd.py
@@ -1,6 +1,7 @@
 a=[4,3,6,33,64,23,26]
 count_even = 0
 
+#counting even numbers
 for i in a :
     if i%2==0:
         print(i)
@@ -8,7 +9,7 @@ for i in a :
     
 print(count_even)
 
-
+#counting odd numbers
 count_odd = 0
 
 for i in a :
@@ -19,12 +20,13 @@ for i in a :
 print(count_odd)
 
 #even index element
-
+print("even index from the array")
 for i in range(0,len(a)):
     if i%2==0:
         print(a[i])
 
 #odd index element
+print("odd index from the array")
 for i in range(0,len(a)):
     if i%2==1:
         print(a[i])
