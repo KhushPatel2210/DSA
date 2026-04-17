0b4d0456c79b57ea384e49701ecc99ac8ffffdb3
khush patel <khushp2210@gmail.com>
2026-04-17 15:28:58 +0530

    reverse array

---
+++ array/revrse.py
@@ -0,0 +1,14 @@
+a=[4,3,6,33,64,23,26]
+reverse=[]
+
+# in-built method
+# a.reverse()
+# print(a)
+
+
+
+for i in range(len(a),0,-1):
+    print(a[i-1])
+    reverse.append(a[i-1])
+
+print(reverse)
