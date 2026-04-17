208bdd78881c088ba43aa6da88026d800e68208f
khush patel <khushp2210@gmail.com>
2026-04-17 14:50:18 +0530

    another logic for the max element

--- array/max-element.py
+++ array/max-element.py
@@ -1,12 +1,16 @@
 a=[4,3,6,33,64,23]
 
-max_elemet = 0
+max_elemet = a[0]
 # print(max_elemet)
-k=len(a)
-print(k)
+# k=len(a)
+# print(k)
 
-for i in range(1,k) :
-    if a[i]>max_elemet:
-        max_elemet = a[i]
+# for i in range(1,k) :
+#     if a[i]>max_elemet:
+#         max_elemet = a[i]
+
+for i in a :
+    if i>max_elemet:
+        max_elemet = i
 
 print(max_elemet)
