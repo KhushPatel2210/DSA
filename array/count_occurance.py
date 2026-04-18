8353bc1cce8118fa34f99b36a0cf46ab74942cf5
khush patel <khushp2210@gmail.com>
2026-04-18 15:27:27 +0530

    full count method

--- array/count_occurance.py
+++ array/count_occurance.py
@@ -1,11 +1,19 @@
 
 # normal count occurance
 a=[2,6,35,2,64,29,64,2]
+freq = {}
 
 count = 0
 for i in a :
     if i == 2:
         count += 1
 print(count)
+
+for i in a :
+    if i in freq :
+        freq[i] +=1
+    else :
+        freq[i] = 1
+print(freq)
 
 # automatic for all no. count is pending
